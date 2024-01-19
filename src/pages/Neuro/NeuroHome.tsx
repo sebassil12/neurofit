1e0bafddd8f2561ddfb83f59a1e4e3599e6ce04a
sebassil12 <anddysilva15@gmail.com>
2024-01-18 22:51:21 -0500

    bajar peso de imagenes

--- src/pages/Neuro/NeuroHome.tsx
+++ src/pages/Neuro/NeuroHome.tsx
@@ -1,14 +1,14 @@
 import NeuroCardHome from "../../components/neuroApp/NeuroCardHome";
-import { APP_HOME } from "../../constants/constants";
+import { APP_HOME, IMAGE_CAROUSEL } from "../../constants/constants";
 import apphome from './styles/apphome.module.css'
 export default function NeuroHome() {
   return (
     <section className={apphome.container}>
       <section>
-        <NeuroCardHome title={"Plan de ejercicios"} description={"Ejercicios acorde a tus necesidades"} pathImage={APP_HOME.ejercicio} pathLink={"/neuro/buy"} />
+        <NeuroCardHome title={"Plan de ejercicios"} description={"Ejercicios acorde a tus necesidades"} pathImage={IMAGE_CAROUSEL.fisio} pathLink={"/neuro/buy"} />
       </section>
       <section>
-        <NeuroCardHome title={"Kit de entrenamiento"} description={"Encuentra todas las herramientas para tus ejercicios"} pathImage={APP_HOME.kit} pathLink={"/neuro/buy"} />
+        <NeuroCardHome title={"Kit de entrenamiento"} description={"Encuentra todas las herramientas para tus ejercicios"} pathImage={IMAGE_CAROUSEL.old_man} pathLink={"/neuro/buy"} />
       </section>
     </section>
   )
